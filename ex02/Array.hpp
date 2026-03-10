41e2b4b464cf202f434b267fc9e478c4a7047819
Tomasz Szymanski <tszymans@student.42.fr>
2026-03-10 22:15:09 +0100

    [ex02] Final corrections and functions added

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Array.hpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tszymans <tszymans@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/10 19:28:24 by tszymans          #+#    #+#             */
-/*   Updated: 2026/03/10 20:18:07 by tszymans         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
@@ -21,21 +9,21 @@ class Array {
 	public:
 		Array() : _data(NULL), _size(0) {}
 		Array(unsigned int n) : _data(new T[n]()), _size(n) {} // () at the end is a C++98 trick that ensures value-initialization (setting integers to 0, for example)
-		Array(const Array& src) : _data(NULL), _size(n) {
+		Array(const Array& src) : _data(NULL), _size(0) {
 			*this = src;
 		}
 		Array& operator=(const Array& rhs) {
-			if(*this!=&rhs){
+			if(this != &rhs){
 				//1. Delete old memory
 				delete [] _data;
 				//4. Update _size
-				_size = other._size;
+				_size = rhs._size;
 				if (_size > 0) {
 					//2. Allocate new memory based on other._size
 					_data = new T[_size]();
 					//3. Copy element from other._data to new _data
 					for (unsigned int i = 0; i < _size; i++){
-						_data[i] = other._data[i];
+						_data[i] = rhs._data[i];
 					}
 				} else {
 					_data = NULL;
@@ -48,7 +36,23 @@ class Array {
 				delete[] _data;
 		}
 
-		unsigned int size();
+		// Subscript operator for non-const objects
+		T & operator[](unsigned int index){
+			if (index >= _size)
+				throw std::exception(); //Subject requirement
+			return _data[index];
+		}
+
+		// Subscript operator for const objects
+		const T & operator[](unsigned int index) const {
+			if (index >= _size)
+				throw std::exception();
+			return _data[index];
+		}
+
+		unsigned int size() const{
+			return _size;
+		}
 };
 
 #endif
